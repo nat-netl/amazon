4e4d37e9e1d7e4d788bfc98761c82ce8b7171fcc
nat-netl <nat_netl3@bk.ru>
2022-07-18 14:12:58 +0300

    amazon-commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDQHki0guuO4AGtaXexgMUlefTzbi_XeKs",
+  authDomain: "e-clone-44dec.firebaseapp.com",
+  projectId: "e-clone-44dec",
+  storageBucket: "e-clone-44dec.appspot.com",
+  messagingSenderId: "113153580670",
+  appId: "1:113153580670:web:b524a43abfc27ce63e422f",
+  measurementId: "G-VT0NTZ9KKR"
+};
