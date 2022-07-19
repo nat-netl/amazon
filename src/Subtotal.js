a4dc842eada3f9f7cff143c757ee4bb8806b3546
nat-netl <nat_netl3@bk.ru>
2022-07-19 15:11:55 +0300

    main

---
+++ src/Subtotal.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import CurrencyFormat from 'react-currency-format'
+import './Subtotal.css'
+
+function Subtotal() {
+  return (
+    <div className="subtotal">
+
+      <CurrencyFormat
+            renderText={(value) =>(
+                <>
+                    <p>
+                        Subtotal(0 items):
+                        <strong>0</strong>
+                    </p>
+                    <small className="subtotal-gift">
+                        <input type="checkbox" /> this order contains a gift
+                    </small>
+                </>
+            )}
+            decimalScale={2}
+            value={0}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+      />
+      <button>Proceed to checkout</button>
+    </div>
+  )
+}
+
+export default Subtotal
